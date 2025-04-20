e3b160cae6ed5bb403bfd1995336e74a235f6b79
MacTii <mactii41@gmail.com>
2025-04-20 21:56:43 +0200

    Remove duplicate node function

--- src/js/modules/animations.js
+++ src/js/modules/animations.js
@@ -58,12 +58,4 @@ export function initAnimations() {
     ...scrollRevealOption,
     delay: 1500,
   });
-
-  const instagram = document.querySelector(".instagram__flex");
-
-  Array.from(instagram.children).forEach((item) => {
-    const duplicateNode = item.cloneNode(true);
-    duplicateNode.setAttribute("aria-hidden", true);
-    instagram.appendChild(duplicateNode);
-  });
 }
